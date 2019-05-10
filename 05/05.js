7396b5e267a45bef5e6d4950bea755043d892490
Ben Patterson <ben.patterson@avnet.com>
2019-05-10 09:13:55 -0400

    Exercise 05

---
+++ 05/05.js
@@ -0,0 +1,27 @@
+/**
+ * EXERCISE 5
+ * FILTERED LS
+ * 
+ * Create a program that prints a list of files in a given directory,
+ * filtered by the extension of the files. You will be provided a directory
+ * name as the first argument to your program (e.g. '/path/to/dir/') and a
+ * file extension to filter by as the second argument.
+ *
+ * For example, if you get 'txt' as the second argument then you will need to
+ * filter the list to only files that end with .txt. Note that the second
+ * argument will not come prefixed with a '.'.
+ *
+ * Keep in mind that the first arguments of your program are not the first
+ * values of the process.argv array, as the first two values are reserved for
+ * system info by Node.
+ *
+ * The list of files should be printed to the console, one file per line. You
+ * must use asynchronous I/O.
+ */
+const fs = require('fs')
+const path = require('path')
+const ext = process.argv[3]
+fs.readdir(process.argv[2], (err, list) => {
+    if (err) console.error(err)
+    console.log(list.filter(file => path.extname(file).substring(1) === ext).join('\n'))
+})
